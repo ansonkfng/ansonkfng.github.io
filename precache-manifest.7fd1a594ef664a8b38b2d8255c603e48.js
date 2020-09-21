fa88b7293eff55a2e6dbe2554a1bf72fe67c07dc
Anson Ng <ansonng@astri.org>
2020-09-21 23:34:39 +0800

    Initial commit

---
+++ precache-manifest.7fd1a594ef664a8b38b2d8255c603e48.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8860b691234ea4de0d99c257f5149304",
+    "url": "/sushiswap-prototype/index.html"
+  },
+  {
+    "revision": "fe9e2078a855301dff39",
+    "url": "/sushiswap-prototype/static/css/main.550685a2.chunk.css"
+  },
+  {
+    "revision": "11177c3aa6f4a6a0a1ba",
+    "url": "/sushiswap-prototype/static/js/0.03ca3c7a.chunk.js"
+  },
+  {
+    "revision": "947eecd542cecdda9c7e",
+    "url": "/sushiswap-prototype/static/js/10.f8539bc6.chunk.js"
+  },
+  {
+    "revision": "3e4c819b0fb5d0f3f09a",
+    "url": "/sushiswap-prototype/static/js/11.38306c0b.chunk.js"
+  },
+  {
+    "revision": "4f6b51a1197418aac533",
+    "url": "/sushiswap-prototype/static/js/3.08606425.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/sushiswap-prototype/static/js/3.08606425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107accbe19f39980a939",
+    "url": "/sushiswap-prototype/static/js/4.67f239fb.chunk.js"
+  },
+  {
+    "revision": "f72a7d6ffc618cc2815d",
+    "url": "/sushiswap-prototype/static/js/5.35d5e371.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/sushiswap-prototype/static/js/5.35d5e371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f30ed86927166b8c05",
+    "url": "/sushiswap-prototype/static/js/6.05746cdc.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/sushiswap-prototype/static/js/6.05746cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50637daeca7e7181b0c",
+    "url": "/sushiswap-prototype/static/js/7.9bcc3499.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/sushiswap-prototype/static/js/7.9bcc3499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ae9f32233452d8339a",
+    "url": "/sushiswap-prototype/static/js/8.55fe61b3.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/sushiswap-prototype/static/js/8.55fe61b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35138aa29a96a1f962a",
+    "url": "/sushiswap-prototype/static/js/9.298e5412.chunk.js"
+  },
+  {
+    "revision": "fe9e2078a855301dff39",
+    "url": "/sushiswap-prototype/static/js/main.269f1969.chunk.js"
+  },
+  {
+    "revision": "04e1e6563d35a6e8d089",
+    "url": "/sushiswap-prototype/static/js/runtime-main.57396585.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "/sushiswap-prototype/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/sushiswap-prototype/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/sushiswap-prototype/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
---
+++ static/js/11.38306c0b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsushiswap-prototype"]=this["webpackJsonpsushiswap-prototype"]||[]).push([[11],{529:function(p,s){}}]);
+//# sourceMappingURL=11.38306c0b.chunk.js.map
